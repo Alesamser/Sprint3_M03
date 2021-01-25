e8a72d5a6f8cdcd970ebb2139c34aec307e4c01f
Alesamser <73219965+Alesamser@users.noreply.github.com>
2021-01-25 13:26:50 +0100

    Update Menu.java
    Suprimir

--- Menu.java
+++ Menu.java
@@ -11,10 +11,10 @@ public class Menu {
         agenda[2]= "Gomez López";
         agenda[3]= "Muñoz Alonso";
         nAlumnes = 4;
-        System.out.println("Tria una opció:\n1-Inserir 2-Localitzar 3-Recuperar 4-Suprimir \n5-SuprimirDada 6-Anul·lar 7-PrimerDarrer 8-Imprimir \n9-Ordenar 10-LocalitzarEnOrdenada 11-SuprimirDadaEnOrdenada 12-Sortir");
         Scanner teclat = new Scanner(System.in);
         int c = 1;
             do{
+                System.out.println("Tria una opció:\n1-Inserir 2-Localitzar 3-Recuperar 4-Suprimir \n5-SuprimirDada 6-Anul·lar 7-PrimerDarrer 8-Imprimir \n9-Ordenar 10-LocalitzarEnOrdenada 11-SuprimirDadaEnOrdenada 12-Sortir");
                 int n = teclat.nextInt();
                 switch (n){
                     case 1: System.out.println("Inserir");
@@ -38,7 +38,40 @@ public class Menu {
                     break;
                     case 3: System.out.println("Recuperar");
                     break;
-                    case 4: System.out.println("Suprimir");
+                    case 4: 
+                        if (nAlumnes!=0){
+                        System.out.println("Introdueix una posició.");
+                        int p = teclat.nextInt();
+                            if (p>nAlumnes || p<=0){
+                                System.out.println("Posició no valida.");
+                            }
+                            else {
+                                System.out.println("Estàs segur/a que vols eliminar l'alumne de la posició "+p+"?\n1 Per a sí       2 Per a no");
+                                int o = teclat.nextInt();
+                                if (o==1){
+                                    if (nAlumnes<20){
+                                        for (int i = p-1;i<nAlumnes;i++){
+                                            agenda[i]=agenda[i+1];
+                                        }
+                                        nAlumnes--;
+                                        System.out.println("Alumne de la posició "+p+" borrat amb exit.");
+                                    }
+                                    else{
+                                        nAlumnes--;
+                                    }
+                                    System.out.println("Llista actualitzada:");
+                                    for(int i = 0; i<nAlumnes;i++){
+                                        System.out.println(agenda[i]);
+                                    }
+                                }
+                                else{
+                                    System.out.println("Borrat de la posició "+p+" cancel·lat.");
+                                }
+                            }
+                        }
+                        else{
+                            System.out.println("No hi ha alumnes a la llista.");
+                        }
                     break;
                     case 5: System.out.println("SuprimirDada");
                     break;
